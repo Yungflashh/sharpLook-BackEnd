9147ec3555cbc3b1d626bd649989c050c2535438
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-13 23:31:28 +0100

    added the review route

---
+++ src/routes/review.routes.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { postReview, fetchVendorReviews } from "../controllers/review.controller"
+import { verifyToken } from "../middlewares/auth.middleware"
+
+const router = express.Router()
+
+router.post("/postReview", verifyToken, postReview)
+router.get("/:vendorId", fetchVendorReviews)
+
+export default router
