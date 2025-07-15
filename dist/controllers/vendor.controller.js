44e1fa9bc2e9182b1d07507209ecb4689139b43d
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-15 12:29:14 +0100

    adding vendor detaILS

--- dist/controllers/vendor.controller.js
+++ dist/controllers/vendor.controller.js
@@ -9,7 +9,11 @@ const vendor_services_1 = require("../services/vendor.services");
 const cloudinary_1 = __importDefault(require("../utils/cloudinary"));
 const completeVendorProfile = async (req, res) => {
     try {
-        const updated = await (0, vendorOnboarding_service_1.updateVendorProfile)(req.user.id, req.body);
+        const { userId, ...profileData } = req.body;
+        if (!userId) {
+            return res.status(400).json({ error: "Missing userId in request body" });
+        }
+        const updated = await (0, vendorOnboarding_service_1.updateVendorProfile)(userId, profileData);
         res.json({ success: true, message: "Profile updated", data: updated });
     }
     catch (err) {
