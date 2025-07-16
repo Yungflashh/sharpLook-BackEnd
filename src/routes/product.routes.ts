63fe422f0f0629250659d43f73d74615a6fefaaa
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-16 23:11:42 +0100

    debbuging servcies and edit product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -2,21 +2,23 @@ import express from "express"
 import { addProduct, fetchVendorProducts,fetchAllProducts, editProduct, removeProduct} from "../controllers/product.controller"
 import { verifyToken, requireRole } from "../middlewares/auth.middleware"
 import { uploadSingle2 } from "../middlewares/upload.middleware"
+import multer from "multer"
 
 const router = express.Router()
+const upload = multer({ storage: multer.memoryStorage() })
 
 
 router.post(
   "/vendor/addProducts",
   verifyToken,
   requireRole(["VENDOR"]),
-  uploadSingle2,
+   upload.single("picture"),
   addProduct
 )
 
 router.get("/getVendorProducts",verifyToken,requireRole(["VENDOR"]),fetchVendorProducts)
 router.get("/getAllProducts", fetchAllProducts)
-router.put("/:productId", verifyToken, uploadSingle2, requireRole(["VENDOR"]), editProduct)
-router.delete("/:productId", verifyToken,requireRole(["VENDOR"]), removeProduct)
+router.put("/edit/:productId", verifyToken,  upload.single("picture"), requireRole(["VENDOR"]), editProduct)
+router.delete("/delete/:productId", verifyToken,requireRole(["VENDOR"]), removeProduct)
 
 export default router
