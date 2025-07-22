cffe8638195f57fdb9f62cdb599cd347346e4df4
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-22 23:43:46 +0100

    working thru msg

--- dist/routes/message.routes.js
+++ dist/routes/message.routes.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/routes/message.routes.ts
 const express_1 = require("express");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
 const message_controller_1 = require("../controllers/message.controller");
 const router = (0, express_1.Router)();
-router.get("/:roomId", message_controller_1.fetchMessages);
-router.patch("/:roomId/read", message_controller_1.markAsRead);
-router.patch("/:messageId/like", message_controller_1.likeMessage);
-router.get("/unread/count", message_controller_1.getUnreadMessageCount);
+router.get("/:roomId", auth_middleware_1.verifyToken, message_controller_1.fetchMessages);
+router.patch("/:roomId/read", auth_middleware_1.verifyToken, message_controller_1.markAsRead);
+router.patch("/:messageId/like", auth_middleware_1.verifyToken, message_controller_1.likeMessage);
+router.get("/unread/count", auth_middleware_1.verifyToken, message_controller_1.getUnreadMessageCount);
 exports.default = router;
--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -7,13 +7,14 @@ import {
   likeMessage,
   getUnreadMessageCount
 } from "../controllers/message.controller"
+import { objectEnumValues } from "@prisma/client/runtime/library"
 
 const router = Router()
 
-router.get("/:roomId", fetchMessages)
-router.patch("/:roomId/read", markAsRead)
-router.patch("/:messageId/like", likeMessage)
-router.get("/unread/count", getUnreadMessageCount)
+router.get("/:roomId", verifyToken, fetchMessages)
+router.patch("/:roomId/read", verifyToken, markAsRead)
+router.patch("/:messageId/like", verifyToken, likeMessage)
+router.get("/unread/count", verifyToken, getUnreadMessageCount)
 
 
 export default router
