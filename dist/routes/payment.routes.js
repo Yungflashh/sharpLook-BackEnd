810d689505e0c27bcd63b33e55212179451ae574
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-23 09:30:36 +0100

    fixed the payment

--- dist/routes/payment.routes.js
+++ dist/routes/payment.routes.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const payment_controlelr_1 = require("../controllers/payment.controlelr");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
 const router = express_1.default.Router();
-router.post("/paystack/initiate", payment_controlelr_1.createPaystackPayment);
-router.get("/paystack/verify/:reference", payment_controlelr_1.verifyPaystackPayment);
+router.post("/paystack/initiate", auth_middleware_1.verifyToken, payment_controlelr_1.createPaystackPayment);
+router.get("/paystack/verify/:reference", auth_middleware_1.verifyToken, payment_controlelr_1.verifyPaystackPayment);
 exports.default = router;
--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -3,10 +3,11 @@ import {
   createPaystackPayment,
   verifyPaystackPayment,
 } from "../controllers/payment.controlelr";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
-router.post("/paystack/initiate", createPaystackPayment);
-router.get("/paystack/verify/:reference", verifyPaystackPayment);
+router.post("/paystack/initiate", verifyToken, createPaystackPayment);
+router.get("/paystack/verify/:reference", verifyToken, verifyPaystackPayment);
 
 export default router;
