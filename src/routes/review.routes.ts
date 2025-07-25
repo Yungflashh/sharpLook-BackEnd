a47f2858381da1f66d7d348601583fd1c2122246
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-26 00:16:55 +0100

    fixed the review issues

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -1,20 +1,12 @@
 import express from "express"
-import { fetchVendorReviews, handleGetServiceReviewsByVendor, handleGetProductReviewsByVendor} from "../controllers/review.controller"
+import { postReview, fetchVendorReviews } from "../controllers/review.controller"
 import { verifyToken } from "../middlewares/auth.middleware"
-import { postBookingReview, postProductReview, postServiceReview } from '../controllers/review.controller';
 
 const router = express.Router()
 
 
-// router.post("/postReview", verifyToken, postReview)
-router.post('/reviews/booking', postBookingReview);
-router.post('/product', postProductReview);
-router.post('/reviews/service', postServiceReview);
+router.post("/postReview", verifyToken, postReview)
 router.post("/getAllReviews", fetchVendorReviews)
-router.get("/:vendorId/service/:serviceId/reviews", handleGetServiceReviewsByVendor);
-router.get("/:vendorId/product/:productId/reviews", handleGetProductReviewsByVendor);
-
-
 
 export default router
 
