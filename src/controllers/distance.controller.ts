d40585cba1bf6a7c6840e5e93628b3f8641e4106
streaktv8@gmail.com <streaktv8@gmail.com>
2025-07-26 22:03:06 +0100

    fixed the distance calculator

--- src/controllers/distance.controller.ts
+++ src/controllers/distance.controller.ts
@@ -18,10 +18,26 @@ export const calculateDistance =  (req: Request, res: Response) => {
   }
 
   const distanceKm = haversineDistanceKm(clientLat, clientLng, vendorLat, vendorLng);
-  // const homeServicePrice = calculateHomeServicePrice(distanceKm);
+  const totalKm = Math.round(distanceKm)
+  const tranportPrice = totalKm * 200;
 
+  console.log(tranportPrice);
+  
   return res.json({
-    distanceKm: Number(distanceKm.toFixed(2)),
+    transportPrice: Number(tranportPrice),
    
   });
 };
+
+
+// let clientLat = 40.7128
+
+// let clientLng =  -74.0060
+// let vendorLat =  30.0444
+
+
+// let vendorLng =   31.2357
+
+// let answer = haversineDistanceKm(clientLat,clientLng, vendorLat, vendorLng )
+// let whole  =Math.round(answer)
+// console.log( whole * 200);
