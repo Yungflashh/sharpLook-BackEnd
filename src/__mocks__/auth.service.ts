1c1ecfebaf07fa85d52607ff466e333a99dd973f
streaktv8@gmail.com <streaktv8@gmail.com>
2025-08-11 10:10:35 +0100

    disuptes error fixeds 2

--- src/__mocks__/auth.service.ts
+++
@@ -1,7 +0,0 @@
-export const registerUser = jest.fn();
-export const loginWithVendorCheck = jest.fn();
-export const loginWithClientCheck = jest.fn();
-export const loginUser = jest.fn();
-export const requestPasswordReset = jest.fn();
-export const resetPassword = jest.fn();
-export const getUserById = jest.fn();
--- src/__mocks__/otp.service.ts
+++
@@ -1,5 +0,0 @@
-import * as authService from "../services/auth.service";
-import * as otpService from "../services/otp.service";
-
-jest.mock("../services/auth.service");
-jest.mock("../services/otp.service");
